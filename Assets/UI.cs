9ca765b5185150e36467a1637f68575c8523ec0d
Evidemment <toinou41210@gmail.com>
2018-04-24 16:43:14 +0200

    ui

---
+++ Assets/UI.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour {
+
+
+    #region Declaration
+
+    // Static Data
+    public GameObject SelectedTower;
+    public GameObject TargetTower;
+
+    // Dynamic Data
+
+
+    // Subscripts
+    private Tower_Hub Hub;
+	private UnitSpawner Spawner;
+
+    #endregion
+
+
+    // AWAKE
+    void Awake() {
+        //InitializeData();
+    }
+
+    // START
+    void Start() {
+        InitializeScript();
+    }
+
+    // UPDATE
+    void Update() {
+    }
+
+	#region Getters
+
+
+    #endregion
+
+
+	#region Event
+
+
+    #endregion
+
+
+	#region Methods
+    public void totale()
+	{
+		Spawner.Move(TargetTower.transform,100);
+	}
+	public void troisq()
+	{
+		Spawner.move(TargetTower.transform,75);
+	}
+	public void moitier()
+	{
+		Spawner.move(TargetTower.transform,50);
+	}
+	public void unq()
+	{
+        Spawner.move(TargetTower.transform,25);
+	}	
+	public void Labo()
+	{
+		Hub.ChangeType(TowerData.BuildingType.Lab);
+	}
+	public void Generateru()
+	{
+		Hub.ChangeType(TowerData.BuildingType.Generator);
+	}
+	public void levelup()
+	{
+		Hub.LevelUP();
+	}
+	public void parefeu()
+	{
+        Hub.ChangeType(TowerData.BuildingType.Firewall);
+	}
+
+	#endregion
+
+
+	#region Subfunctions
+    
+	//private void InitializeData() { }
+	private void InitializeScripts() {
+		Hub = SelectedTower.GetComponent<Tower_Hub>();
+		Spawner = SelectedTower.GetComponent<UnitSpawner>();
+     }
+    //private void InitializeRules() { }
+    
+    #endregion
+}
