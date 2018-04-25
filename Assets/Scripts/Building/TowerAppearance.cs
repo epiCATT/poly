f803032158882ea01db52170530a99e52192b7e2
Griffures <geotony550@gmail.com>
2018-04-25 14:55:42 +0200

    Implémentation des Modèles (TowerAppearance)

--- Assets/Scripts/Building/TowerAppearance.cs
+++ Assets/Scripts/Building/TowerAppearance.cs
@@ -7,7 +7,14 @@ public class TowerAppearance : MonoBehaviour {
     #region Declaration
 
     // Static Data
-    private Renderer renderer;
+    public Mesh LabModel;
+    public Mesh GeneratorModel;
+    public Mesh FirewallModel;
+    public Material Material1;
+    public Material Material2;
+    public Material Material3;
+    private Renderer mrenderer;
+    private MeshFilter mfilter;
 
     // Dynamic Data
     private Color controllerColor;
@@ -39,20 +46,9 @@ public class TowerAppearance : MonoBehaviour {
 
     public void ActualizeBody()
     {
-        controllerColor = Data.ControllerData.Color;
-        
-        // Change tower's color
-        renderer.material.color = controllerColor;
-
-        // Change Etiquette Color
-        //UnitText.color = controllerColor;
-
-        // Change Particle color
-        //Particle.startColor = controllerColor;
-
-        // TO DO : Change tower appearrance
-
-        // TO DO : Change tower's largeness
+        UpdateModel();
+        UpdateMaterial();
+        UpdateColor();
     }
 
     #endregion
@@ -62,12 +58,63 @@ public class TowerAppearance : MonoBehaviour {
 
     private void InitializeData()
     {
-        renderer = GetComponent<Renderer>();
+        mrenderer = GetComponent<Renderer>();
+        mfilter = GetComponent<MeshFilter>();
     }
 
     private void InitializeScripts()
     {
         Data = GetComponentInParent<TowerData>();
+    }
+
+    private void UpdateModel()
+    {
+        switch (Data.Type)
+        {
+            case TowerData.BuildingType.Generator:
+                mfilter.mesh = GeneratorModel;
+                break;
+            case TowerData.BuildingType.Firewall:
+                mfilter.mesh = FirewallModel;
+                break;
+            case TowerData.BuildingType.Lab:
+                mfilter.mesh = LabModel;
+                break;
+            default:
+                throw new System.ArgumentException("Type not Initialized.");
+        }
+    }
+
+    private void UpdateMaterial()
+    {
+        switch (Data.Level)
+        {
+            case 0:
+                mrenderer.material = Material1;
+                break;
+            case 1:
+                mrenderer.material = Material2;
+                break;
+            case 2:
+                mrenderer.material = Material3;
+                break;
+            default:
+                throw new System.ArgumentException("Level out of bound.");
+        }
+    }
+
+    private void UpdateColor()
+    {
+        controllerColor = Data.ControllerData.Color;
+
+        // Change tower's color
+        mrenderer.material.color = controllerColor;
+
+        // Change Etiquette Color
+        //UnitText.color = controllerColor;
+
+        // Change Particle color
+        //Particle.startColor = controllerColor;
     }
 
     #endregion
