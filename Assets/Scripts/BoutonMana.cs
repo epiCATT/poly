0183ed0410ae3b04bfaa0debacc820328d2a33e4
Evidemment <toinou41210@gmail.com>
2018-04-25 17:54:01 +0200

    Implementation des menus.

---
+++ Assets/Scripts/BoutonMana.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BoutonMana : MonoBehaviour {
+
+	public  void SingleBtn(string PlayScene)
+    {
+        SceneManager.LoadScene(PlayScene);
+    }
+    public void MultiBtn(string PlayScene)
+    {
+        SceneManager.LoadScene(PlayScene);
+    }
+    public void OptionBtn(string PlayScene)
+    {
+        SceneManager.LoadScene(PlayScene);
+    }
+
+    public void exitbtn()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Settings : MonoBehaviour {
+
+
+   public void return2(string PlayScene)
+    {
+        SceneManager.LoadScene(PlayScene);
+    }
+}
