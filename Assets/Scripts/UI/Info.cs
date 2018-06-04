adce89a52589374622a7180f03cbeb359454e1fc
Evidemment <toinou41210@gmail.com>
2018-06-04 13:51:29 +0200

    Info

---
+++ Assets/Scripts/UI/Info.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Info : MonoBehaviour {
+    [SerializeField] public Image customeimage;
+    void Start() {
+        customeimage.enabled = false;
+    }
+    public void On()
+    {
+        if(customeimage.enabled == false)
+        {
+            customeimage.enabled = true;
+        }
+        else
+        {
+            customeimage.enabled = false;
+        }
+
+    }
+
+    
+}
